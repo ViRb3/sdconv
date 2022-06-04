b60bf276fa0b61ecd159283c7426f863cec8539b
Victor <virb3e@gmail.com>
2022-06-04 22:41:36 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,59 @@
+# sdconv
+
+> Automatic SD video conversion script
+
+## Usage
+
+1. Install Hybrid and run it at least once so it creates its directories.
+2. Use the script.
+
+```bash
+sdconv.py [-h] [-o OUT_DIR] [--rename | --no-rename] [--encode | --no-encode] [-c CUTOFF_SIZE] [--profile PROFILE] [--preset PRESET] INPUT [INPUT ...]
+
+Automatic SD video conversion script.
+
+positional arguments:
+  INPUT                 File or directory to convert. If a directory, will merge all files inside as chapters of a single file. If you want to glob, use PowerShell: '(Get-Item E:\*)[0..20]'.
+
+options:
+  -h, --help            show this help message and exit
+  -o OUT_DIR, --out-dir OUT_DIR
+                        Directory where to save converted files. (default: .)
+  --rename, --no-rename
+                        Rename files according to their modification timestamp. (default: False)
+  --encode, --no-encode
+                        Encode the video after deinterlacing. (default: True)
+  -c CUTOFF_SIZE, --cutoff-size CUTOFF_SIZE
+                        Skip files smaller than this size in MB. (default: 5)
+  --profile PROFILE     Hybrid profile to use when deinterlacing. (default: profiles/pal.xml)
+  --preset PRESET       Handbrake preset to use when encoding. (default: presets/x265.json)
+```
+
+## Process
+
+1. Deinterlace video/s using QTGMC in Hybrid. Output as x265 NVEnc lossless, since it has the smallest size and highest speed, and also allows color space hinting (check caveats section).
+2. If renaming is enabled, rename the output files to their modification time. This will be used to generate chapter names next.
+3. If a folder with multiple videos was input, merge the output files into a single video via mkvmerge, generating chapters from each file's name.
+4. If encoding is not disabled, encode the video/s using Handbrake.
+
+## Caveats
+
+1. QTGMC, at least in Hybrid, always interprets PAR as 1:1, so it produces skewed video dimensions from non-square input PAR videos. To mitigate, convert the video's PAR to 1:1 prior to deinterlacing. Note that with PAL, the width will grow proportionally, while in NTSC, the height.
+
+2. Hybrid, by default, won't output color primaries and transfer hints when rendering. Handbrake then has no choice but to try and guess them from the video's framrate. If you apply frame bobbing when deinterlacing and double the framerate to 50, since it is >25 (PAL), the video will be deemed NTSC, and the wrong color primaries and transfer will be used. To mitigate this, make Hybrid encode to H265, and always output these hints. Alternatively, force Handbrake to use the correct color primaries and transfer via its custom x265 arguments option.
+
+## Encoder observations
+
+All encoders below were compared at settings with equivalent encoding speed and output size.
+
+### x264
+
+Preserves noise and detail better, but causes rapidly shifting, blocky artifacts. Plays everywhere.
+
+### x265
+
+Smoothens noise and detail, but results in significantly less, very evenly distributed, non-blocky artifacts. Plays on Safari and Edge. Requires codec on default Windows player.
+
+### AV1
+
+30% faster encoding than the rest, while preserving the most noise and detail, has a moderate amount of rapidly shifting, blocky artifacts. Does not play on Safari or QuickTime. Requires codec on default Windows player.
---
+++ sdconv.py
@@ -0,0 +1,200 @@
+import json
+from pathlib import Path
+import argparse
+import subprocess
+import os
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+from typing import Any, Collection, List
+import datetime
+import xml.etree.ElementTree as ET
+
+profile_dir = Path(os.getenv("APPDATA", default="")).resolve() / "hybrid/profiles/global"
+handbrake = Path("HandBrakeCLI.exe").resolve()
+hybrid_root = Path(os.getenv("PROGRAMFILES", default="")).resolve() / "Hybrid"
+hybrid = (hybrid_root / "Hybrid.exe").resolve()
+ffmpeg = (hybrid_root / "64bit" / "ffmpeg.exe").resolve()
+mkvmerge = (hybrid_root / "64bit" / "mkvmerge.exe").resolve()
+
+for f in [profile_dir, handbrake, hybrid_root, hybrid, ffmpeg, mkvmerge]:
+    if not f.exists():
+        raise Exception(f"Unable to access required file or directory: {f}")
+
+
+def intersperse(lst: Collection[Any], item: Any):
+    result = [item] * (len(lst) * 2 - 1)
+    result[0::2] = lst
+    return result
+
+
+def run_hybrid(profile: Path, out_dir: Path, *inputs: Path):
+    with TemporaryDirectory(dir=out_dir, prefix="hybrid-temp-") as temp_dir:
+        with NamedTemporaryFile(dir=profile_dir, prefix="profile-", suffix=".xml", delete=False) as profile_temp:
+            try:
+                root = ET.parse(profile)
+                defaultOutputPath = root.find(".//HybridData[@name='defaultOutputPath']")
+                if defaultOutputPath is None:
+                    raise Exception("Can't find defaultOutputPath in profile")
+                defaultOutputPath.set("value", str(out_dir))
+                defaultTempPath = root.find(".//HybridData[@name='defaultTempPath']")
+                if defaultTempPath is None:
+                    raise Exception("Can't find defaultTempPath in profile")
+                defaultTempPath.set("value", str(temp_dir))
+                root.write(profile_temp)
+                profile_temp.close()
+                subprocess.run(
+                    [hybrid, "-global", Path(profile_temp.name), *inputs, "-autoAdd", "addAndStart"]
+                ).check_returncode()
+            finally:
+                os.remove(profile_temp.name)
+
+
+def run_handbrake(preset: Path, output: Path, input: Path):
+    with open(preset) as f:
+        preset_name = json.load(f)["PresetList"][0]["PresetName"]
+    subprocess.run(
+        [
+            handbrake,
+            "-i",
+            input,
+            "-o",
+            output,
+            "--preset-import-file",
+            preset,
+            "--preset",
+            preset_name,
+        ]
+    ).check_returncode()
+
+
+def run_mkvmerge(output: Path, *inputs: Path):
+    subprocess.run(
+        [
+            mkvmerge,
+            "-o",
+            output,
+            "--generate-chapters",
+            "when-appending",
+            "--generate-chapters-name-template",
+            "<FILE_NAME>",
+            *intersperse(inputs, "+"),
+        ]
+    ).check_returncode()
+
+
+def should_skip(file: Path, cutoff_size: int) -> bool:
+    return file.stat().st_size < cutoff_size * 1_000_000
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Automatic SD video conversion script.", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "inputs",
+        metavar="INPUT",
+        nargs="+",
+        type=str,
+        help="File or directory to convert. If a directory, will merge all files inside as chapters of a single file. If you want to glob, use PowerShell: '(Get-Item E:\\*)[0..20]'.",
+    )
+    parser.add_argument(
+        "-o",
+        "--out-dir",
+        type=str,
+        default=".",
+        help="Directory where to save converted files.",
+    )
+    parser.add_argument(
+        "--rename",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Rename files according to their modification timestamp.",
+    )
+    parser.add_argument(
+        "--encode",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="Encode the video after deinterlacing.",
+    )
+    parser.add_argument(
+        "-c",
+        "--cutoff-size",
+        default=5,
+        type=int,
+        help="Skip files smaller than this size in MB.",
+    )
+    parser.add_argument(
+        "--profile",
+        type=str,
+        default="profiles/pal.xml",
+        help="Hybrid profile to use when deinterlacing.",
+    )
+    parser.add_argument(
+        "--preset",
+        type=str,
+        default="presets/x265.json",
+        help="Handbrake preset to use when encoding.",
+    )
+
+    args = parser.parse_args()
+    inputs: List[Path] = [Path(a).resolve() for a in args.inputs]
+    output_dir: Path = Path(args.out_dir).resolve()
+    rename: bool = args.rename
+    cutoff_size: int = args.cutoff_size
+    profile: Path = Path(args.profile)
+    preset: Path = Path(args.preset)
+    encode: bool = args.encode
+
+    for input in [*inputs, output_dir, profile, preset]:
+        if not input.exists():
+            raise Exception(f"Invalid path: {input}")
+
+    for input in inputs:
+        print(f"Processing {input}")
+        if input.is_file():
+            src_files = [input]
+        elif input.is_dir():
+            src_files = input.glob("*")
+        else:
+            raise Exception(f"Unknown input type: {input}")
+
+        src_files = [f for f in src_files if not should_skip(f, cutoff_size)]
+        if len(src_files) < 1:
+            continue
+
+        with TemporaryDirectory(dir=output_dir, prefix="hybrid-conv-") as converted_dir:
+            converted_dir = Path(converted_dir)
+
+            print(f"Running Hybrid...")
+            run_hybrid(profile, converted_dir, *src_files)
+
+            if rename:
+                print(f"Renaming files...")
+                for conv_file in converted_dir.glob("*"):
+                    src_file = next(f for f in src_files if conv_file.stem == f.stem)
+                    ts = datetime.datetime.fromtimestamp(src_file.stat().st_mtime)
+                    new_file = conv_file.with_stem(
+                        f"{ts.year}-{ts.month:02d}-{ts.day:02d} {ts.hour:02d}_{ts.minute:02d}_{ts.second:02d}"
+                    )
+                    conv_file.rename(new_file)
+                    print(f"{conv_file.name} -> {new_file.name}")
+
+            if len(list(converted_dir.glob("*"))) > 1:
+                print(f"Merging files...")
+                conv_files = sorted(converted_dir.glob("*"))
+                merged_file = conv_files[0].with_suffix(".merged.mkv")
+                run_mkvmerge(merged_file, *conv_files)
+                for f in conv_files:
+                    os.remove(f)
+            else:
+                merged_file = next(converted_dir.glob("*"))
+
+            if encode:
+                print(f"Running Handbrake...")
+                final_file = merged_file.with_suffix(".final.mp4")
+                run_handbrake(preset, final_file, merged_file)
+                final_file.rename(output_dir / final_file.name)
+            else:
+                merged_file.rename(output_dir / merged_file.name)
+
+    print("Done")
